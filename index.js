f9141b2050066baa293d61343bd20154deed1abf
shreayasinghkunwar <shreaya@fusemachines.com>
2022-12-16 13:22:03 +0545

    Connecting Db and Api

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ const port = 5500;
 app.use(express.static('public'));
 app.use(express.json());
 
+connection.once("open", function () {
+    console.log("MongoDB database connection established successfully");
+});
+
+async function emptyTable() {
+    const del = await connection.dropCollection("Board")
+    console.log('del', del)
+}
+
+app.post('/api', async (req, res) => {
+    try {
+        emptyTable();
+        const data = req.body;
+        const insertData = await Board.create(data);
+        res.json({
+            status: 'recieved',
+            datas: insertData
+        });
+    } catch (e) {
+        res.send(e);
+    }
+})
 
 
 app.listen(port, () => {
